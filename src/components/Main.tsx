00c0dcf5d40603ce8ea4beda945d6eb0167e72ae
Zahra <bghzahra@outlook.com>
2022-07-30 18:46:08 +0100

    added toolbar and container

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,21 @@ import { PhotoCamera } from "@material-ui/icons"; //imports camera icon
 export default function Main() {
   return (
     <>
-
+        <CssBaseline/>
+        <AppBar position="relative"/>
+        <Toolbar> 
+            <PhotoCamera/> 
+            <Typography variant="h6">
+                Photo Album
+            </Typography>
+        </Toolbar>
+        <main>
+            <div>
+                <Container maxWidth="sm"> 
+                    <Typography variant="h2" align="center" ></Typography>
+                </Container>
+            </div>
+        </main>
     </>
   );
 }
