0614fec547cdcaa88c2e377244c83e8427f5a60d
Zahra <bghzahra@outlook.com>
2022-07-30 19:58:34 +0100

    added use styles function

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,19 +11,35 @@ import {
   Container,
   Button
 } from "@material-ui/core"; //any text element
-import { BurstMode, PhotoCamera } from "@material-ui/icons"; //imports camera icon
+import { PhotoCamera } from "@material-ui/icons"; //imports camera icon
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme)=> ({
+    container: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8,0,6)
+    },
+    icon: {
+        marginRight: '20px'
+    },
+    buttons:{
+        marginTop:'40px'
+    } 
+}))
 
 export default function Main() {
+  const classes = useStyles();
+
   return (
     <>
       <CssBaseline />
       <AppBar position="relative" />
       <Toolbar>
-        <PhotoCamera />
+        <PhotoCamera className={classes.icon}/> 
         <Typography variant="h6">Photo Album</Typography>
       </Toolbar>
       <main>
-        <div>
+        <div className={classes.container}> 
           <Container maxWidth="sm">
             <Typography
               variant="h2"
@@ -43,13 +59,20 @@ export default function Main() {
               consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
               labore et dolore magna aliqua.
             </Typography>
+            <div className={classes.buttons}>
             <Grid container spacing={2} justify="center">
                 <Grid item>
-                    <Button variant="contained" color="primary">
+                    <Button variant="contained" color="primary" >
                         See my photos
+                    </Button>
+                </Grid> 
+                <Grid item>
+                    <Button variant="outlined" color="primary">
+                        Secondary action
                     </Button>
                 </Grid>
             </Grid>
+            </div> 
           </Container>
         </div>
       </main>
