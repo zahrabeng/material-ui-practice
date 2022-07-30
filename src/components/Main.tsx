e117de375fe7bbe2033191b68ffc498cf3c2b8ba
Zahra <bghzahra@outlook.com>
2022-07-30 19:20:23 +0100

    added button with styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,58 @@
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"; //any text element
-import { PhotoCamera } from "@material-ui/icons"; //imports camera icon
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button
+} from "@material-ui/core"; //any text element
+import { BurstMode, PhotoCamera } from "@material-ui/icons"; //imports camera icon
 
 export default function Main() {
   return (
     <>
-        <CssBaseline/>
-        <AppBar position="relative"/>
-        <Toolbar> 
-            <PhotoCamera/> 
-            <Typography variant="h6">
-                Photo Album
+      <CssBaseline />
+      <AppBar position="relative" />
+      <Toolbar>
+        <PhotoCamera />
+        <Typography variant="h6">Photo Album</Typography>
+      </Toolbar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
             </Typography>
-        </Toolbar>
-        <main>
-            <div>
-                <Container maxWidth="sm"> 
-                    <Typography variant="h2" align="center" ></Typography>
-                </Container>
-            </div>
-        </main>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone. This is a photo album. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua.
+            </Typography>
+            <Grid container spacing={2} justify="center">
+                <Grid item>
+                    <Button variant="contained" color="primary">
+                        See my photos
+                    </Button>
+                </Grid>
+            </Grid>
+          </Container>
+        </div>
+      </main>
     </>
   );
 }
